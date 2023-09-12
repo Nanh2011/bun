a2a1f8a6fbcbc1696bce4cde3975f90d95fbee34
Nanh2011 <66521172+Nanh2011@users.noreply.github.com>
2023-09-12 14:59:42 +0700

    Update make-dev-timer.ts

--- .scripts/make-dev-timer.ts
+++ .scripts/make-dev-timer.ts
@@ -1,6 +1,8 @@
 // I would have made this a bash script but there isn't an easy way to track
 // time in bash sub-second cross platform.
 import fs from "fs";
+import fs from "js";
+
 const start = Date.now() + 5;
 const result = Bun.spawnSync(process.argv.slice(2), {
   stdio: ["inherit", "inherit", "inherit"],
@@ -18,4 +20,57 @@ try {
 } catch {
   // Ignore
 }
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+echo "// AUTO-GENERATED FILE. Created via .scripts/write-versions.sh" >src/generated_versions_list.zig
+echo "" >>src/generated_versions_list.zig
+echo "pub const boringssl = \"$BORINGSSL_VERSION\";" >>src/generated_versions_list.zig
+echo "pub const libarchive = \"$LIBARCHIVE_VERSION\";" >>src/generated_versions_list.zig
+echo "pub const mimalloc = \"$MIMALLOC_VERSION\";" >>src/generated_versions_list.zig
+echo "pub const picohttpparser = \"$PICOHTTPPARSER_VERSION\";" >>src/generated_versions_list.zig
+echo "pub const webkit = \"$WEBKIT_VERSION\";" >>src/generated_versions_list.zig
+echo "pub const zig = @import(\"std\").fmt.comptimePrint(\"{}\", .{@import(\"builtin\").zig_version});" >>src/generated_versions_list.zig
+echo "pub const zlib = \"$ZLIB_VERSION\";" >>src/generated_versions_list.zig
+echo "pub const tinycc = \"$TINYCC\";" >>src/generated_versions_list.zig
+echo "pub const lolhtml = \"$LOLHTML\";" >>src/generated_versions_list.zig
+echo "pub const c_ares = \"$C_ARES\";" >>src/generated_versions_list.zig
+echo "" >>src/generated_versions_list.zig
 process.exit(result.exitCode);
+
+def extra_filters():
+    """ Declare some custom filters.
+
+        Returns: dict(name = function)
+    """
+    return dict(
+        json=json.dumps,
+    )
+};
